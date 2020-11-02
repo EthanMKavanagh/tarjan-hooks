f62469db64cc3798b78d5e5d7633a26ea8e53ecb
Edan Schwartz <edanschwartz@gmail.com>
2020-11-02 15:11:10 -0600

    Simplify landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,22 +7,9 @@ import ReduxHooksExample from '../ReduxHooksExample/ReduxHooksExample';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hooks!!!</h1>
       <HooksExample />
-      <ReduxHooksExample />
+      {/* <ReduxHooksExample /> */}
     </div>
   );
 }
